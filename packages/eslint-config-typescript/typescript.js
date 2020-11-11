41086a67090a8b66e11990b8cb5004ad08dcad13
yhbao <yhbao@yhbit.net>
2020-11-11 15:14:12 +0800

    feat(eslint-config-typescript): add noUnusedVars

--- packages/eslint-config-typescript/typescript.js
+++ packages/eslint-config-typescript/typescript.js
@@ -23,5 +23,7 @@ module.exports = {
     "@typescript-eslint/no-use-before-define": ["error"],
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": ["error"],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
   },
 };
