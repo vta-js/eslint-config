875d03080bba54f1e4e6d3263f1806fea9107495
yhbao <yhbao@yhbit.net>
2020-11-11 11:49:08 +0800

    feat(eslint-config-typescript): add noUseBeforeDefine and noShadow

--- packages/eslint-config-typescript/typescript.js
+++ packages/eslint-config-typescript/typescript.js
@@ -18,5 +18,10 @@ module.exports = {
     "plugin:import/typescript",
     "prettier/@typescript-eslint",
   ],
-  rules: {},
+  rules: {
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+  },
 };
